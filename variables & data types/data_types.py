ce34c4741491f44185edaa9d2ea1a8d16061e9b6
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2026-04-20 09:01:27 +0600

    python data types

---
+++ variables & data types/data_types.py
@@ -0,0 +1,9 @@
+first_name = "Rafiul"    # string type data
+last_name = "Islam"    # string type data
+
+a = 10    # integer type data
+b = 10.25    # float type data
+
+food = True    # boolean type data
+hide = False    # boolean type data
+
