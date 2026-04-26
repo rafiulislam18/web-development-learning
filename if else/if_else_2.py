0af906bf6e4bd6a15a0dbbb15a6d4e53cfbb42a1
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2026-04-26 10:59:43 +0600

    if else day 2

---
+++ if else/if_else_2.py
@@ -0,0 +1,6 @@
+age = 15
+
+if age >= 18:
+    print("Tumi vote dite parba")
+else:
+    print("Tumi ekhono choto, vote dite parba na")
---
+++ if else/if_else_3.py
@@ -0,0 +1,26 @@
+# Hafiz vai ekjon student
+# Tar Bangla subject er grade ber korte hobe
+# 80+ hole A+
+# 70-79 hole A
+# 60-69 hole A-
+# 50-59 hole B
+# 40-49 hole C
+# 33-39 hole D
+# 0-32 hole F
+
+bangla = 75
+
+if bangla >= 80:
+    print("Hafiz vai er grade A+")
+elif bangla >= 70:
+    print("Hafiz vai er grade A")
+elif bangla >= 60:
+    print("Hafiz vai er grade A-")
+elif bangla >= 50:
+    print("Hafiz vai er grade B")
+elif bangla >= 40:
+    print("Hafiz vai er grade C")
+elif bangla >= 33:
+    print("Hafiz vai er grade D")
+else:
+    print("Hafiz vai er grade F")
